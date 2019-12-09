89afc3f8395d937bbe8ec310a1ef3e06af6ed74d
CesarMarroquin <19003301@galileo.edu>
2019-12-09 20:55:26 +0000

    users listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,9 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
     
+    def index 
+        @users = User.all
+    end
     
     private 
     def user_params
